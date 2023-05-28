acf6d02160538fc37afcb8a3fbff24795194f82e
CodeByTanvir01~ <tanvirh484@icloud.com>
2023-05-28 23:24:15 +0600

    first commit

---
+++ variable.js
@@ -0,0 +1,7 @@
+var firstline = 'I am going to be'
+var secondline = ' an awesome web developer'
+var secondline = secondline
+
+var combineTheLines = firstline + secondline;
+
+console.log(combineTheLines)
